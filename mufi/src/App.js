84b8a042e10842302959d2ac9f3dd27c0ab3161c
sohee <sthgml@naver.com>
2023-10-22 00:44:50 +0900

    🌱 chore: GlobalStyle, Router 설정

--- mufi/src/App.js
+++ mufi/src/App.js
@@ -1,5 +1,39 @@
+import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import reset from 'styled-reset'
+import Router from "./router/Router";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+body {
+  font-family: 'Pretendard'
+}
+
+@font-face {
+  font-family: 'Pretendard';
+  src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+      format('woff');
+  font-weight: 400;
+  font-style: normal;
+} 
+
+button {
+  font: inherit;
+  font-weight: bold;
+}
+`
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </>
+  )
+  ;
 }
 
 export default App;

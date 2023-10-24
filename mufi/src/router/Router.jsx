b637104b44c9d977c2880d28a9d24cf4b315d79a
깃허브ID5866syhpjy <qkrthdud33333@gmail.com>
2023-10-24 10:11:06 +0900

    ✨ feat: 로그인 페이지 화면 구현

--- mufi/src/router/Router.jsx
+++ mufi/src/router/Router.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Landing from "../pages/landing/Landing";
-import Splash from "../pages/splash/Splash";
+ import Splash from "../pages/splash/Splash";
+import Login from "../pages/login/Login";
 
 export default function Router() {
   return (
     <Routes>
       <Route path='/splash' element={<Splash />} />
       <Route path='/' element={<Landing />} />
+     <Route path="/login" element={<Login />} />
+
+
     </Routes>
   );
 }

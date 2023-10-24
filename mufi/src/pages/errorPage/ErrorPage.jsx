30e5807945718751610f8b36fcf616c082789e7b
깃허브ID5866syhpjy <qkrthdud33333@gmail.com>
2023-10-25 01:06:13 +0900

    ✨ feat: page-404 수정

---
+++ mufi/src/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import {
+  Layout
+} from './ErrorPageStyle';
+import icon404Img from '../../assets/icon-404.png';
+
+export default function ErrorPage() {
+  return (
+    <Layout>
+        <img src={icon404Img} alt="404 Error Icon" />
+    <div>페이지를 찾을 수 없습니다. :</div>
+    </Layout>
+  )
+}

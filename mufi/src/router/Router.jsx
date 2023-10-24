757b3f98967adb3eb2cd4bb34281a284254a0232
박소희 <sthgml@naver.com>
2023-10-24 14:08:40 +0900

    ✨ feat: HomeEmpty 페이지 추가

--- mufi/src/router/Router.jsx
+++ mufi/src/router/Router.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Landing from '../pages/landing/Landing';
 import Splash from '../pages/splash/Splash';
-import SignUpPage from '../pages/signup/SignUpPage';
+import Home from '../pages/home/Home';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/splash" element={<Splash />} />
       <Route path="/" element={<Landing />} />
-      <Route path="/signup" element={<SignUpPage />} />
+      <Route path='/home' element={<Home />} />
     </Routes>
   );
 }

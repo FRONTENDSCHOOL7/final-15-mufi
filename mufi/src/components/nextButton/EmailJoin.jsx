e04c9877174ccb76b83e46a1a1dc0118aa1af708
깃허브ID5866syhpjy <qkrthdud33333@gmail.com>
2023-10-25 03:29:17 +0900

    ✨ feat: 로그인 페이지 수정

---
+++ mufi/src/components/nextButton/EmailJoin.jsx
@@ -0,0 +1,10 @@
+
+
+import React from 'react'
+import { EmailJoinStyle } from '../nextButton/EmailJoinStyle';
+
+export default function EmailJoin() { 
+  return (
+    <EmailJoinStyle>이메일로 회원가입</EmailJoinStyle>
+  )
+}

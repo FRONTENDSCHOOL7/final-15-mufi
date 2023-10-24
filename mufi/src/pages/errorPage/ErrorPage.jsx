6af4f7030883b7a9ab9e2aad5e95263247757cfe
깃허브ID5866syhpjy <qkrthdud33333@gmail.com>
2023-10-25 00:53:22 +0900

    ✨ feat: 404 페이지 구현 수정

--- mufi/src/pages/errorPage/ErrorPage.jsx
+++ mufi/src/pages/errorPage/ErrorPage.jsx
@@ -8,7 +8,7 @@ export default function ErrorPage() {
   return (
     <Layout>
         <img src={icon404Img} alt="404 Error Icon" />
-    <div>페이지를 찾을 수 없습니다. :(</div>
+    <div>페이지를 찾을 수 없습니다. :</div>
     </Layout>
   )
 }

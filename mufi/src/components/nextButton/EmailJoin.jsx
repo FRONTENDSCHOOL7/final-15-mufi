d560b530ec7d348a43ad30ab0810ab746c4cf156
깃허브ID5866syhpjy <qkrthdud33333@gmail.com>
2023-10-25 16:47:52 +0900

    ✨ feat: 로그인 화면 수정

--- mufi/src/components/nextButton/EmailJoin.jsx
+++ mufi/src/components/nextButton/EmailJoin.jsx
@@ -1,10 +1,15 @@
-
-
-import React from 'react'
+import React from 'react';
 import { EmailJoinStyle } from '../nextButton/EmailJoinStyle';
+import { useNavigate } from 'react-router-dom';
 
-export default function EmailJoin() { 
+export default function EmailJoin() {
+  const navigate = useNavigate();
+
+  const onJoin = () => {
+    navigate('/join');
+  }
+
   return (
-    <EmailJoinStyle>이메일로 회원가입</EmailJoinStyle>
-  )
+    <EmailJoinStyle onClick={onJoin}>이메일로 회원가입</EmailJoinStyle>
+  );
 }
